03215a05292b42e9a1153e577a950d2904c267ba
LuisTM7 <luismiguel.tm7@gmail.com>
2025-01-16 17:29:25 -0500

    NoteView creado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,31 @@
-import { Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
+import JournalLayout from "../layout/JournalLayout";
+import NoteView from "../views/NoteView";
+import NothingSelectedView from "../views/NothingSelectedView";
+import { AddOutlined } from "@mui/icons-material";
 
 function JournalPage() {
   return (
     <>
-      <Typography>JournalPage</Typography>
+      <JournalLayout>
+        {/* <Typography variant="h1">Journal</Typography> */}
+        <NothingSelectedView />
+        {/* <NoteView /> */}
+
+        <IconButton
+          size="large"
+          sx={{
+            color: "white",
+            backgroundColor: "error.main",
+            ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+            position: "fixed",
+            right: 50,
+            bottom: 50,
+          }}
+        >
+          <AddOutlined sx={{ fontSize: 30 }} />
+        </IconButton>
+      </JournalLayout>
     </>
   );
 }
