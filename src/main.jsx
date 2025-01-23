2290a5811f515cedf84305080fb525f3be4a2439
LuisTM7 <luismiguel.tm7@gmail.com>
2025-01-23 15:18:36 -0500

    instalacion de redux y creacion de store: authSlice

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import JournalApp from "./JournalApp.jsx";
 import { BrowserRouter } from "react-router";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
