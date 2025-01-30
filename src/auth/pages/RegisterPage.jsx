44caf48bc2b5ceb152b4bbca443f5d6ad9f0cc9c
LuisTM7 <luismiguel.tm7@gmail.com>
2025-01-30 13:39:36 -0500

    validacion de formulario

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,27 +1,100 @@
+import { useMemo, useState } from "react";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "../layout/AuthLayout";
 import { Link as RouterLink } from "react-router";
+import useForm from "../../hooks/useForm";
+import { useDispatch, useSelector } from "react-redux";
+
+const formData = {
+  displayName: "Luis",
+  email: "luis@gmail.com",
+  password: "123456",
+};
+
+const formValidations = {
+  displayName: [(value) => value.length >= 1, "El nombre es obligatorio."],
+  email: [(value) => value.includes("@"), "El correo debe de tener una @"],
+  password: [(value) => value.length >= 6, "El password debe de tener más de 6 letras."],
+};
 
 export const RegisterPage = () => {
+  const dispatch = useDispatch();
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const { status, errorMessage } = useSelector((state) => state.auth);
+  const isCheckingAuthentication = useMemo(() => status === "checking", [status]);
+
+  const {
+    formState,
+    displayName,
+    email,
+    password,
+    onInputChange,
+    isFormValid,
+    displayNameValid,
+    emailValid,
+    passwordValid,
+  } = useForm(formData, formValidations);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    // console.log(formState);
+    setFormSubmitted(true);
+
+    if (!isFormValid) return;
+  };
+
   return (
     <AuthLayout title="Crear cuenta">
-      <form>
+      <h1>FormValid</h1>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label="Nombre completo" type="text" placeholder="Nombre completo" fullWidth />
+            <TextField
+              label="Nombre completo"
+              type="text"
+              placeholder="Nombre completo"
+              fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
+              error={!!displayNameValid && formSubmitted}
+              helperText={displayNameValid}
+            />
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label="Correo" type="email" placeholder="correo@google.com" fullWidth />
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
+              error={!!emailValid && formSubmitted}
+              helperText={emailValid}
+            />
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label="Contraseña" type="password" placeholder="Contraseña" fullWidth />
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
+              error={!!passwordValid && formSubmitted}
+              helperText={passwordValid}
+            />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>
+              <Button disabled={isCheckingAuthentication} type="submit" variant="contained" fullWidth>
                 Crear cuenta
               </Button>
             </Grid>
